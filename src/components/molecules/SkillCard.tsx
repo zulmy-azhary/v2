9d7cc54d9a982e3f89529a76c0f8e1442c3da0ac
zoel-a <zulmyazhary32@gmail.com>
2023-01-25 06:58:13 +0800

    Fix grid layout for mobile screen

--- src/components/molecules/SkillCard.tsx
+++ src/components/molecules/SkillCard.tsx
@@ -9,7 +9,7 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
 
 const SkillCard: React.FC<Props> = ({ title, items, className, ...rest }) => {
   return (
-    <Box className={clsx("max-w-[17rem] h-fit", className)} {...rest}>
+    <Box className={clsx("lg:max-w-[17rem] h-fit", className)} {...rest}>
       <Box className="h-fit"><h4 className="px-4 py-2">{title}</h4></Box>
       <Box className="px-4 py-3">
         {items.map((item, i) => (
--- src/components/organisms/Skills.tsx
+++ src/components/organisms/Skills.tsx
@@ -6,7 +6,7 @@ const Skills: React.FC = () => {
     <Section section={"skills"} headingClassName="basis-5/12">
       <div className="flex flex-col lg:flex-row">
         <div className="basis-2/5"></div>
-        <div className="basis-3/5 grid grid-flow-col grid-rows-2 gap-5">
+        <div className="basis-3/5 grid grid-flow-row lg:grid-flow-col grid-cols-1 lg:grid-rows-2 gap-5">
           <SkillCard title="Languages" items={["Javascript", "Typescript"]} />
           <SkillCard title="Framework/Library" items={["Sass", "ReactJs", "VueJs", "Svelte", "NextJs"]} />
           <SkillCard
