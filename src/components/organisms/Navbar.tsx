99cc7042d327bf2f0273b2c1fcb92b412fda5683
zoel-a <zulmyazhary32@gmail.com>
2023-06-18 18:02:08 +0800

    Refactor Navbar component

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -27,16 +27,14 @@ const Navbar: React.FC = () => {
       <header key={pathname} className="fixed inset-x-0 top-0 z-10 bg-bgColor pt-5 pb-5 md:pt-7 md:pb-3 xl:pt-10">
         <nav className="mx-auto flex max-w-xs items-center justify-between md:max-w-2xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-8xl 3xl:max-w-9xl">
           <BrandLogo />
-          <aside ref={asideRef as React.RefObject<HTMLElement>}>
-            {isTablet ? (
-              <NavList items={chooseItems} />
-            ) : (
-              <>
-                <Sidebar isOpen={isOpen} items={chooseItems} />
-                <Hamburger />
-              </>
-            )}
-          </aside>
+          {isTablet ? (
+            <NavList items={chooseItems} />
+          ) : (
+            <aside ref={asideRef}>
+              <Sidebar isOpen={isOpen} items={chooseItems} />
+              <Hamburger />
+            </aside>
+          )}
         </nav>
       </header>
     </AnimatePresence>
