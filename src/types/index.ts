62098e3a5f08de3d5cd259800d87818b03a25d58
zoel-a <zulmyazhary32@gmail.com>
2024-01-07 14:53:22 +0800

    Add Tech type for techs property from Project and SocialMedia type, update types of Project

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,22 @@
+import type { IconType } from "react-icons";
+
 export type ProjectType = "main" | "unfinished" | "others";
 
+export type Tech = {
+  name: string;
+  url: string;
+};
+
+export type SocialMedia = {
+  iconName: IconType;
+  url: string;
+};
+
 export type Project = {
-  id: number;
   title: string;
   src?: string;
   description?: string;
-  techList: string[];
+  techs: Tech[];
   liveUrl?: string;
   repoUrl: string;
   type: ProjectType;
