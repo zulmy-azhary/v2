9d24ca4f51554c59a5059a5f89d46b9845689440
zoel-a <zulmyazhary32@gmail.com>
2024-01-07 15:10:39 +0800

    add andika's font variable, update color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,16 @@ module.exports = {
     extend: {
       fontFamily: {
         firaCode: "var(--firaCode)",
+        andika: "var(--andika)"
       },
       colors: {
-        bgColor: "#282C33",
-        primary: "#C778DD",
-        gray: "#ABB2BF",
+        bgColor: "#121212",
+        primary: "#6D31EF", //#6D31EF
+        gray: "#FFFFFF",
         accent: "#4338D9",
-        springGreen: "#2AF598",
+        springGreen: "#2AF598", //#50D9B3
         pigmentBlue: "#3437AA",
+        card: "#282C34"
       },
       maxWidth: {
         "8xl": "104rem",
