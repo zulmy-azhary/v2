84a326d744bcea8ab53a007b90a5e348f75d2dca
zoel-a <zulmyazhary32@gmail.com>
2024-01-07 15:46:10 +0800

    Update style of footer content & copyright

--- src/components/molecules/FooterContent.tsx
+++ src/components/molecules/FooterContent.tsx
@@ -7,13 +7,13 @@ const FooterContent: React.FC = () => {
       <div className="flex items-center justify-center gap-x-4 md:justify-start lg:gap-x-6">
         <div className="flex items-center gap-x-1">
           <Logo className="scale-75 stroke-white" />
-          <Text fontWeight="font-semibold" className="text-xl md:text-2xl">
+          <Text fontWeight="font-semibold" className="font-firaCode text-xl md:text-2xl">
             Zoel
           </Text>
         </div>
         <Text className="text-xs text-gray md:text-sm">zulmyazhary32@gmail.com</Text>
       </div>
-      <Text className="ml-2 text-center text-sm md:text-start lg:text-base xl:w-5/6">
+      <Text className="ml-2 text-center text-sm tracking-wider md:text-start lg:text-base xl:w-5/6">
         Web designer and front-end developer based on Makassar, Indonesia
       </Text>
     </div>
--- src/components/molecules/FooterCopyright.tsx
+++ src/components/molecules/FooterCopyright.tsx
@@ -4,7 +4,7 @@ import { Text } from "@/components/atoms";
 
 const FooterCopyright: React.FC = () => {
   return (
-    <Text className="col-span-2 self-center justify-self-center text-center text-sm text-gray lg:text-base">
+    <Text className="col-span-2 self-center justify-self-center text-center text-sm tracking-wide text-gray lg:text-base">
       Created with <FaHeart className="inline-block text-red-500" /> by Zulmy Azhary AS
     </Text>
   );
