012dc421f2dcfaac5d1e019aa0c4ad349daf352a
zoel-a <zulmyazhary32@gmail.com>
2024-01-07 15:30:46 +0800

    Add code block for hero section, update hero content

---
+++ src/components/molecules/HeroCodeBlock.tsx
@@ -0,0 +1,97 @@
+import { PrismAsync as SyntaxHighlighter } from "react-syntax-highlighter";
+import theme from "react-syntax-highlighter/dist/cjs/styles/prism/one-dark";
+import React, { useRef } from "react";
+import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
+
+/*
+  Life is a blank canvas, paint it with the colors of your experiences.
+  Each stroke tells a story, each shade holds a memory.
+  The masterpiece is not the destination, but the journey of creation.
+*/
+
+const code = `function createSystem(): System {
+  return new System();
+};
+
+function gatherFeatures(): Feature[] {
+  return Design.getAllFeatures();
+};
+
+function applyFeature(system: System, feature: Feature) {
+  const { aesthetics, functionality } = feature;
+  
+  system.addAesthetics(aesthetics);
+  system.addFunctionality(functionality);
+};
+
+function buildSystem(): System {
+  const system = createSystem();
+  const features = gatherFeatures();
+
+  features.forEach(feature => {
+    applyFeature(system, feature);
+  });
+
+  return system;
+};
+
+const elegantSystem = buildSystem();`;
+
+const HeroCodeBlock: React.FC = () => {
+  const cardRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const mouseXSpring = useSpring(x, { stiffness: 75 });
+  const mouseYSpring = useSpring(y, { stiffness: 75 });
+
+  const rotateX = useTransform(mouseYSpring, [-0.5, 0.5], ["8deg", "-8deg"]);
+  const rotateY = useTransform(mouseXSpring, [-0.5, 0.5], ["-8deg", "8deg"]);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!cardRef.current) return;
+    const rect = (cardRef.current as HTMLElement).getBoundingClientRect();
+
+    const width = rect.width;
+    const height = rect.height;
+
+    const mouseX = (e.clientX - rect.left) / width - 0.5;
+    const mouseY = (e.clientY - rect.top) / height - 0.5;
+
+    x.set(mouseX);
+    y.set(mouseY);
+  };
+
+  const handleMouseLeave = () => {
+    x.set(0);
+    y.set(0);
+  };
+
+  return (
+    <div className="w-full text-xs md:min-h-[27rem] md:text-base lg:basis-3/5 lg:text-sm xl:basis-1/2 xl:text-base 2xl:basis-4/12">
+      <motion.div
+        ref={cardRef}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        style={{
+          rotateX,
+          rotateY,
+          transformStyle: "preserve-3d"
+        }}
+        className="relative rounded bg-card/30"
+      >
+        <SyntaxHighlighter
+          language="typescript"
+          style={theme}
+          customStyle={{
+            transform: "translateZ(75px)",
+            transformStyle: "preserve-3d"
+          }}
+        >
+          {code}
+        </SyntaxHighlighter>
+      </motion.div>
+    </div>
+  );
+};
+
+export default HeroCodeBlock;
--- src/components/molecules/HeroContent.tsx
+++ src/components/molecules/HeroContent.tsx
@@ -3,18 +3,22 @@ import { ButtonLink, Text, TextHighlight } from "@/components/atoms";
 
 const HeroContent: React.FC = () => {
   return (
-    <div className="flex flex-col items-center gap-y-5 md:items-start md:gap-y-8 lg:basis-3/5 xl:gap-y-10">
-      <h1 className="text-2xl font-medium leading-7 tracking-tight md:text-center md:leading-10 lg:text-start lg:text-3xl xl:text-4xl 2xl:text-5xl">
-        I am Zoel, a <TextHighlight>web designer</TextHighlight> and <TextHighlight>front end developer</TextHighlight>
+    <div className="flex flex-col items-center gap-y-5 md:items-start md:gap-y-8 lg:basis-2/5 xl:basis-1/2 xl:gap-y-10">
+      <h1 className="text-4xl font-bold tracking-tight md:text-center md:text-5xl lg:text-start lg:text-4xl xl:text-6xl xl:leading-[1.1] 2xl:text-5xl">
+        Building a bridge between <TextHighlight className="text-sky-400">design</TextHighlight> and{" "}
+        <TextHighlight className="text-red-400">functionality</TextHighlight>, weaving elegance into{" "}
+        <TextHighlight className="text-springGreen">code</TextHighlight>
       </h1>
-      <Text className="w-full text-justify text-sm tracking-wide text-gray md:text-center md:text-base lg:text-start lg:text-lg xl:w-11/12 2xl:text-2xl">
-        Design, building, and maintaining interactive and responsive websites especially for modern websites.
+      <Text className="w-full text-justify text-sm tracking-wide text-gray md:text-center md:text-lg lg:text-start xl:w-11/12 2xl:text-xl">
+        Combining design aesthetics with intricate technical expertise and becomes the architectural blueprint for
+        immersive, user-centered digital landscapes. From pixel to interaction, each line intricately weaves elegance
+        into functionality, creating an unforgettable digital experience that goes beyond utility.
       </Text>
       <ButtonLink
         variant="primary"
-        href="https://resume.io/r/B7lYHzQFx"
+        href="/assets/CV.pdf" //https://elegantcv.app/resume/8aa0d00c-44ea-4c1d-8277-75e6bf97dad9/summary
         target="_blank"
-        className="self-end md:self-center lg:self-auto"
+        className="self-end px-6 py-3 md:self-center lg:self-auto"
       >
         Download CV
       </ButtonLink>
