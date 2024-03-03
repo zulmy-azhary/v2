9eefea4823cb55c447ef1f302ea610c7c9c26f09
zoel-a <zulmyazhary32@gmail.com>
2024-03-04 04:37:14 +0800

    refactor: add media query list's custom hook

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -15,3 +15,27 @@ export const useMediaQuery = (query: string): boolean => {
 
   return matches;
 };
+
+// For multiple media queries
+type MediaQueryListProps<TValues> = {
+  isDesktop: TValues;
+  isLaptop: TValues;
+  isTablet: TValues;
+  isMobile: TValues;
+};
+
+export const useMediaQueryList = <TValues extends React.ReactNode>({
+  isDesktop,
+  isLaptop,
+  isTablet,
+  isMobile
+}: MediaQueryListProps<TValues>) => {
+  const isDesktopQuery = useMediaQuery("(min-width: 1440px)");
+  const isLaptopQuery = useMediaQuery("(min-width: 1024px)");
+  const isTabletQuery = useMediaQuery("(min-width: 768px)");
+
+  if (isDesktopQuery) return isDesktop;
+  if (isLaptopQuery) return isLaptop;
+  if (isTabletQuery) return isTablet;
+  return isMobile;
+};
