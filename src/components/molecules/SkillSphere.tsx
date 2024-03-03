d7ef61379fa308ceb5094a43052c51855c4af72e
zoel-a <zulmyazhary32@gmail.com>
2024-03-04 04:41:32 +0800

    refactor: use media query list's custom hook to get radius size

--- src/components/molecules/SkillSphere.tsx
+++ src/components/molecules/SkillSphere.tsx
@@ -1,28 +1,26 @@
 import Image from "next/image";
 import { TagSphere } from "@/components/atoms";
 import { allSkills as skills } from "@/data/skills";
-import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { useMediaQueryList } from "@/hooks/useMediaQuery";
 
 const SkillSphere: React.FC = () => {
-  const isTablet = useMediaQuery("(min-width: 768px)");
-  const radiusSize = isTablet ? 270 : 175;
-  const imageSize = isTablet ? 48 : 32;
+  const radiusSize = useMediaQueryList<number>({ isDesktop: 270, isLaptop: 200, isTablet: 300, isMobile: 175 });
 
   const skillItems = skills.map((skill) => {
     return (
-      <Image
-        key={skill.name}
-        className="xxl:w-16 w-8 select-none object-contain md:w-10"
-        src={skill.url}
-        width={imageSize}
-        height={imageSize}
-        priority
-        alt="Skill Icon"
-      />
+      <div key={skill.name} className="relative h-7 w-7 object-cover md:h-9 md:w-9 lg:h-8 lg:w-8 xl:h-9 xl:w-9">
+        <Image className="select-none" src={skill.url} fill priority alt="Skill Icon" />
+      </div>
     );
   });
 
-  return <TagSphere radius={radiusSize} className="z-0 md:basis-1/2" texts={skillItems} />;
+  return (
+    <TagSphere
+      radius={radiusSize}
+      className="z-0 md:basis-1/2 md:self-center lg:basis-2/5 xl:basis-5/12"
+      texts={skillItems}
+    />
+  );
 };
 
 export default SkillSphere;
